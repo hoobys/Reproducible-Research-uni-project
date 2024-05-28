fb7b64b9b766eaacd2bc17e62ad99e9aa7ce8578
Hubert Wojewoda <hubertwojewoda@wp.pl>
2024-05-28 15:46:14 +0200

    plotting AUC

--- rr_project/visualization.py
+++ rr_project/visualization.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, auc
+import os
+import joblib
 
 
 @dataclass
@@ -28,3 +30,21 @@ def get_roc_for_multiple_models(*args: ModelInfo) -> plt.Figure:
 
     ax.legend(loc="lower right")
     return fig
+
+
+def load_models_and_generate_roc(
+    models_directory: str, X_test: np.ndarray, y_test: np.ndarray
+) -> plt.Figure:
+    model_infos = []
+    for file_name in os.listdir(models_directory):
+        if file_name.endswith(".pkl"):
+            model_path = os.path.join(models_directory, file_name)
+            model = joblib.load(model_path)
+            y_proba = model.predict_proba(X_test)[:, 1]
+            model_name = file_name.replace(".pkl", "")
+            model_info = ModelInfo(
+                y_proba=y_proba, y_true=y_test, model_name=model_name
+            )
+            model_infos.append(model_info)
+
+    return get_roc_for_multiple_models(*model_infos)
