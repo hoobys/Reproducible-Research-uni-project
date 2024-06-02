18f15f3a5e372894acbb6f6519e989c63f4a3347
Jakub Zmujdzin <jzmujdzin@gmail.com>
2024-06-02 19:02:15 +0200

    hyperopt model train funs

--- rr_project/train_models.py
+++ rr_project/train_models.py
@@ -1,11 +1,13 @@
+from dataclasses import dataclass, field
+
 import joblib
-import numpy as np
-from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.tree import DecisionTreeClassifier
-from xgboost import XGBClassifier
+import pandas as pd
+from loguru import logger
+from sklearn.base import BaseEstimator
+from sklearn.compose import ColumnTransformer, make_column_selector
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.preprocessing import OneHotEncoder, RobustScaler
 
 from rr_project.config.const import SEED
 
@@ -25,40 +27,135 @@ def save_model(pipeline: Pipeline, model_name: str) -> None:
     print(f"Model {model_name} has been trained and saved successfully.")
 
 
-def train_and_save_models(
-    X_train: np.ndarray,
-    y_train: np.ndarray,
-    scaler: StandardScaler = StandardScaler(),
+def get_pipeline_for_model(model, model_params: dict = None):
+    numerical_prep = make_pipeline(RobustScaler())
+    categorical_prep = make_pipeline(
+        OneHotEncoder(handle_unknown="ignore", sparse=False, drop="first"),
+    )
+    preprocess = ColumnTransformer(
+        [
+            (
+                "numerical",
+                numerical_prep,
+                make_column_selector(dtype_include=["int64", "float64"]),
+            ),
+            (
+                "categorical",
+                categorical_prep,
+                make_column_selector(dtype_include=object),
+            ),
+        ],
+        remainder="passthrough",
+    )
+    preprocess.set_output(transform="pandas")
+    return Pipeline(
+        [
+            ("preprocess", preprocess),
+            ("model", model(**model_params if model_params else {})),
+        ]
+    )
+
+
+@dataclass
+class OneModelHyperoptResult:
+    best_model: BaseEstimator
+    best_score: float
+    cv_results: pd.DataFrame
+
+    def get_model_name(self) -> str:
+        return self.best_model["model"].__class__.__name__
+
+
+@dataclass
+class HyperoptInput:
+    model: BaseEstimator
+    hyperopt_space: dict = field(default_factory=dict)
+
+
+@dataclass
+class HyperoptResults:
+    results: list
+
+    def __post_init__(self):
+        self._sort_by_best_score()
+
+    def _sort_by_best_score(self, reversed: bool = True):
+        self.results.sort(key=lambda x: x.best_score, reverse=reversed)
+
+    def get_best_model(self):
+        return self.results[0].best_model
+
+    def get_best_score(self):
+        return self.results[0].best_score
+
+    def get_merged_df(self):
+        results = pd.DataFrame()
+        for result in self.results:
+            results = pd.concat(
+                [
+                    results,
+                    result.cv_results.assign(model_name=result.get_model_name()),
+                ],
+                axis=0,
+            )
+
+        return results
+
+    def get_all_dfs(self):
+        return [(result.get_model_name(), result.cv_results) for result in self.results]
+
+    def get_all_scores(self):
+        return [(result.get_model_name(), result.best_score) for result in self.results]
+
+    def get_all_models(self):
+        return [(result.get_model_name(), result.best_model) for result in self.results]
+
+
+def run_hyperopt_one_model(
+    x: pd.DataFrame,
+    y: pd.Series,
+    model_input: HyperoptInput,
+    n_iter: int = 10,
+    cv: int = 5,
     random_state: int = SEED,
-    suffix: str = "",
-) -> None:
-    """
-    Train and save specified models using a scikit-learn pipeline.
+):
+    pipeline = get_pipeline_for_model(model_input.model)
+    search = RandomizedSearchCV(
+        pipeline,
+        model_input.hyperopt_space,
+        n_iter=n_iter,
+        scoring="average_precision",
+        n_jobs=-1,
+        cv=cv,
+        random_state=random_state,
+    )
+    search.fit(x, y)
+    return OneModelHyperoptResult(
+        best_model=search.best_estimator_,
+        best_score=search.best_score_,
+        cv_results=pd.DataFrame(search.cv_results_),
+    )
 
-    Parameters:
-    X_train (np.ndarray): Training feature matrix.
-    y_train (np.ndarray): Training target vector.
-    scaler (StandardScaler): Scaler for feature standardization.
 
-    Returns:
-    None
-    """
-    models = {
-        "logistic_regression_model"
-        + suffix: LogisticRegression(random_state=random_state),
-        "decision_tree_model"
-        + suffix: DecisionTreeClassifier(random_state=random_state),
-        "random_forest_model"
-        + suffix: RandomForestClassifier(random_state=random_state),
-        "gradient_boosting_model"
-        + suffix: GradientBoostingClassifier(random_state=random_state),
-        "xgboost_model"
-        + suffix: XGBClassifier(
-            random_state=random_state, use_label_encoder=False, eval_metric="logloss"
-        ),
-    }
-
-    for model_name, model in models.items():
-        pipeline = Pipeline([("scaler", scaler), ("model", model)])
-        pipeline.fit(X_train, y_train)
-        save_model(pipeline, model_name)
+def run_hyperopt(
+    hyperopt_inputs: list,
+    x,
+    y,
+    n_iter: int = 10,
+    cv: int = 5,
+    random_state: int = SEED,
+) -> HyperoptResults:
+    results = []
+    for model_input in hyperopt_inputs:
+        logger.info(f"Running hyperopt for {model_input.model.__name__}")
+        result = run_hyperopt_one_model(
+            x=x,
+            y=y,
+            model_input=model_input,
+            n_iter=n_iter,
+            cv=cv,
+            random_state=random_state,
+        )
+        results.append(result)
+        logger.info(f"Best score: {result.best_score}")
+    return HyperoptResults(results=results)
